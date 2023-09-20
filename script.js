2251aa98363ee84e7a3443ecbf87a68e7f0472dd
wakeem <134740381+hdsnisnotreal@users.noreply.github.com>
2023-09-20 17:41:21 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+const form = document.getElementById("password-form");
+const passwordInput = document.getElementById("password");
+const message = document.getElementById("message");
+
+const validPassword = "continue"; // Replace with your desired password
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const enteredPassword = passwordInput.value;
+
+    if (enteredPassword === validPassword) {
+        window.location.href = "protected-page.html"; // Redirect to the protected page
+    } else {
+        message.textContent = "Incorrect password. Please try again.";
+    }
+});
